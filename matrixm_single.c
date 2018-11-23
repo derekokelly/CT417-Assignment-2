9d064fdd420a8803ca9155221ab10398b37d1708
Derek O'Kelly <derekokelly1997@gmail.com>
2018-11-23 19:04:23 +0000

    add in total time taken for the loops

--- matrixm_single.c
+++ matrixm_single.c
@@ -14,7 +14,7 @@ int main(void)
   int n = MSIZE;
   double pi = 3.141592653589793;
   double s, angle;
-  double wtime1, wtime2, wtime3;
+  double wtime, wtime1, wtime2, wtime3;
 
 
   printf ( "\n" );
@@ -32,6 +32,7 @@ int main(void)
   */
   s = 1.0 / sqrt ( ( double ) ( n ) );
 
+  wtime = omp_get_wtime();
   wtime1 = omp_get_wtime();
 
   for (i = 0; i < n; i++)
@@ -80,11 +81,15 @@ int main(void)
   }
   
   wtime3 = omp_get_wtime ( ) - wtime3;
+  wtime = omp_get_wtime() - wtime;
   
   printf ( "\n" );
   printf ( "  Elapsed time loop 1 = %g seconds\n", wtime1 );
   printf ( "  Elapsed time loop 2 = %g seconds\n", wtime2 );
   printf ( "  Elapsed time loop 3 = %g seconds\n", wtime3 );
+
+  printf ( "\n" );
+  printf ( "  Total lapsed time = %g seconds\n", wtime );
 
   printf ( "\n" );
   printf ( "  Normal end of execution.\n" );
