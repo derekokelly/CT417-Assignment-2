7177e796795a5ba55539abb737e670a613956244
Derek O'Kelly <derekokelly1997@gmail.com>
2018-11-23 17:00:07 +0000

    record times for each loop

--- matrixm_single.c
+++ matrixm_single.c
@@ -14,7 +14,7 @@ int main(void)
   int n = MSIZE;
   double pi = 3.141592653589793;
   double s, angle;
-  double wtime;
+  double wtime1, wtime2, wtime3;
 
 
   printf ( "\n" );
@@ -32,7 +32,7 @@ int main(void)
   */
   s = 1.0 / sqrt ( ( double ) ( n ) );
 
-  wtime = omp_get_wtime();
+  wtime1 = omp_get_wtime();
 
   for (i = 0; i < n; i++)
   {
@@ -42,10 +42,15 @@ int main(void)
       a[i][j] = s * (sin (angle) + cos (angle));
     }
   }
+
+  wtime1 = omp_get_wtime ( ) - wtime1;
 	  
   /*
     Loop 2: Copy A into B.
   */
+
+  wtime2 = omp_get_wtime();
+
   for (i = 0; i < n; i++)
   {
     for (j = 0; j < n; j++)
@@ -54,9 +59,14 @@ int main(void)
     }
   }
 
+  wtime2 = omp_get_wtime ( ) - wtime2;
+
   /*
     Loop 3: Compute C = A * B.
   */
+
+  wtime3 = omp_get_wtime();
+
   for (i = 0; i < n; i++)
   {
     for ( j = 0; j < i; j++ ) // n
@@ -69,9 +79,11 @@ int main(void)
     }
   }
   
-  wtime = omp_get_wtime ( ) - wtime;
+  wtime3 = omp_get_wtime ( ) - wtime3;
   
-  printf ( "  Elapsed time = %g seconds\n", wtime );
+  printf ( "  Elapsed time loop 1 = %g seconds\n", wtime1 );
+  printf ( "  Elapsed time loop 2 = %g seconds\n", wtime2 );
+  printf ( "  Elapsed time loop 3 = %g seconds\n", wtime3 );
 
   printf ( "\n" );
   printf ( "  Normal end of execution.\n" );
